f915fc76760632f954b79b27b54e3cb0fd6e002a
Bharath <bharathc109@gmail.com>
2026-07-08 20:17:46 +0530

    feat(customer-mobile): update shared models and generated files

--- apps/customer-mobile/lib/shared/models/wallet.dart
+++ apps/customer-mobile/lib/shared/models/wallet.dart
@@ -1,11 +1,9 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:json_annotation/json_annotation.dart';
 
 part 'wallet.freezed.dart';
 part 'wallet.g.dart';
 
 @freezed
-@JsonSerializable()
 class Wallet with _$Wallet {
   const factory Wallet({
     required double balance,
@@ -14,5 +12,4 @@ class Wallet with _$Wallet {
   }) = _Wallet;
 
   factory Wallet.fromJson(Map<String, dynamic> json) => _$WalletFromJson(json);
-  Map<String, dynamic> toJson() => _$WalletToJson(this);
 }
